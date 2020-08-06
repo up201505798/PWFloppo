9fed08adfc0981044ec9cfaafa4f2a59350f375e
João Gonçalves <up201505798@fe.up.pt>
2020-08-06 23:11:19 +0100

    Projeto inicial leitura USART INT

---
+++ PWMFloppo.X/newavr-main.c
@@ -0,0 +1,90 @@
+/*
+    (c) 2019 Microchip Technology Inc. and its subsidiaries.
+    Subject to your compliance with these terms, you may use Microchip software and any
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party
+    license terms applicable to your use of third party software (including open source software) that
+    may accompany Microchip software.
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS
+    FOR A PARTICULAR PURPOSE.
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS
+    SOFTWARE.
+*/
+#define F_CPU                           (4000000UL)         /* using default clock 4MHz*/
+#define USART1_BAUD_RATE(BAUD_RATE)     ((float)(64 * 4000000 / (16 * (float)BAUD_RATE)) + 0.5)
+#include <avr/io.h>
+#include <util/delay.h>
+#include<avr/interrupt.h>
+
+void USART1_sendChar(char c);
+
+
+ISR(USART1_RXC_vect){
+    USART1_sendChar( USART1.RXDATAL + 1 ); 
+}
+
+
+void USART1_init(void)
+{
+    PORTC.DIRSET = PIN0_bm;                             /* set pin 0 of PORT C (TXd) as output*/
+    PORTC.DIRCLR = PIN1_bm;                             /* set pin 1 of PORT C (RXd) as input*/
+    
+    USART1.BAUD = (uint16_t)(USART1_BAUD_RATE(9600));   /* set the baud rate*/
+    
+    USART1.CTRLC = USART_CHSIZE0_bm
+                 | USART_CHSIZE1_bm;                    /* set the data format to 8-bit*/
+                 
+    USART1.CTRLA = USART_RXCIE_bm;// | USART_TXCIE_bm;   
+    USART1.CTRLB |= (USART_TXEN_bm |USART_RXEN_bm) ;                      /* enable transmitter*/
+}
+
+void USART1_sendChar(char c)
+{
+    while(!(USART1.STATUS & USART_DREIF_bm))
+    {
+        ;
+    }
+    
+    USART1.TXDATAL = c;
+    while(!(USART1.STATUS & USART_DREIF_bm)) 
+    {
+        ;
+    }
+}
+
+
+
+
+int main (void)
+{
+    
+
+
+
+
+void USART1_init(void);
+
+
+
+    USART1_init();
+    sei();
+    while (1) 
+    {
+         
+        _delay_ms(1000);
+    }
+}
+    
+    
+    
+    
+    
+ 
+    
